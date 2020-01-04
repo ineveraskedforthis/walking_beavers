8a349dee223f6e52bb08b021ed140733eb808a7a
ineveraskedforthis <ineveraskedforthis@users.noreply.github.com>
2020-01-04 22:35:10 +0300

    water, population changes, etc

--- ui/world_explorer.lua
+++ ui/world_explorer.lua
@@ -9,6 +9,7 @@ netr.log = {}
 -- 
 
 function netr.loop_step()
+    netr.log.f("check update draw functions" .. tostring(netr.update) .. tostring(netr.draw), false)
     netr.update()
     netr.draw()
 end
@@ -141,9 +142,19 @@ end
 
 function netr.update()
     netr.log.f("update", netr.log.update)
-    for i = 1, netr.band_count do
+    local total_size = 0
+    for i, tmp in pairs(netr.bands) do
+        netr.log.f("update i" .. tostring(i), netr.log.update)
         netr.bands[i]:update()
+        total_size = total_size + netr.bands[i].size
     end
+    netr.population_text:text(tostring(total_size))
+    for i = 1, netr.bands_to_delete_counter do
+        netr.log.f("delete band " .. netr.bands_to_delete[i], netr.log.update)
+        netr.bands[netr.bands_to_delete[i]] = nil
+    end
+    netr.bands_to_delete = {}
+    netr.bands_to_delete_counter = 0
     netr.log.f("finish update", netr.log.update)
 end
 
@@ -157,8 +168,9 @@ function netr.draw()
     -- end
     netr.restore_colours(true)
     netr.log.f("colours restored", netr.log.draw)
-    for i = 1, netr.band_count do
+    for i, tmp in pairs(netr.bands) do
         netr.log.f("draw i " .. tostring(i), netr.log.draw)
+        netr.log.f("netr.bands[i] " .. tostring(netr.bands[i]), netr.log.draw)
         netr.bands[i]:draw(false)
     end
     netr.save_colours(netr.flags.colour_changed)
@@ -166,12 +178,63 @@ function netr.draw()
     netr.log.f("finish draw", netr.log.draw)
 end
 
+function netr.is_water_fresh(tag)
+    if tag == 'FRESHWATER_LAKE' then
+        return true
+    elseif tag == 'RIVER' then
+        return true
+    elseif tag == 'WETLANDS' then
+        return true
+    end
+    return false
+end
+
+
+--class Stock()
+netr.Stock = {}
+
+function netr.Stock:new()
+    local o = {}
+    setmetatable(o, self)
+    self.__index = self
+    o:init()
+    return o
+end
+
+function netr.Stock:init()
+    self.data = {}
+end
+
+function netr.Stock:set(tag, x)
+    netr.log.f("stock set " .. tag .. " " .. x, netr.log.stock)
+    if x <= 0 then
+        self.data[tag] = nil
+    else
+        self.data[tag] = x
+    end
+end
+
+function netr.Stock:add(tag, x)
+    netr.log.f("stock add " .. tag .. " " .. x, netr.log.stock)
+    self:set(tag, self:get(tag) + x)
+end
+
+function netr.Stock:get(tag)
+    netr.log.f("stock get " .. tag, netr.log.stock)
+    if self.data[tag] == nil then
+        return 0
+    else
+        return self.data[tag]
+    end
+end
+--end of the class Stock() 
+
 
 --class Band()
 netr.Band = {}
 
 function netr.Band:new(id, tile, colour)
-    o = {}
+    local o = {}
     setmetatable(o, self)
     self.__index = self
     o:init(id, tile, colour)
@@ -185,6 +248,17 @@ function netr.Band:init(id, tile, colour)
     self.tile = tile
     self.movement_progress = 0
     self.migration_target = nil
+    self.skills = {}
+    self.skills['agrarian'] = 0
+    self.stock = netr.Stock:new()
+    self.size = 100
+    self.growth_rate = 0.03
+    self.death_rate = 0.02
+    self.growth_progress = 0
+    self.death_progress = 0
+    self.flags = {}
+    self.flags.not_enough_food = false
+    self.flags.not_enough_water = false
     netr.log.f("band base values are ready", netr.log.band.init)
 end
 
@@ -195,12 +269,69 @@ function netr.Band:draw(save)
 end
 
 function netr.Band:update()
+    self:gather_update()
+    self:consume_update()
     self:migration_update()
+    self:stock_update()
+end
+
+function netr.Band:gather_update()
+    netr.log.f("gather_update", netr.log.band.gather)
+    self.stock:add('plants', 500)
+    netr.log.f("plants collected", netr.log.band.gather)
+    local tmp = world.tileWaterbody[self.tile]
+    netr.log.f("local waterbody " .. tostring(tmp), netr.log.band.gather)
+    if tmp ~= 0 then
+        netr.log.f("waterbody active " .. tostring(world.WaterbodyActive[tmp]), netr.log.band.gather)
+        netr.log.f("waterbody type " .. tostring(world.waterbodyType[tmp]), netr.log.band.gather)
+        if world.WaterbodyActive[tmp] and netr.is_water_fresh(world.waterbodyType[tmp]) then
+            self.stock:add('water', self.size)
+        end
+    end
+    netr.log.f("gather_update finished", netr.log.band.gather)
+end
+
+function netr.Band:consume_update()
+    netr.log.f("consume_update", netr.log.band.consume)
+    local tmp = self.size
+    if tmp > self.stock:get('plants') then
+        self.flags.not_enough_food = true
+        tmp = self.stock:get('plants')
+    end
+    self.growth_progress = self.growth_progress + self.growth_rate * tmp
+    self.death_progress = self.death_progress + self.size * self.death_rate
+    netr.log.f("food effects calculated", netr.log.band.consume)
+    if self.stock:get('water') < self.size then
+        self.flags.not_enough_water = true
+        self.death_progress = self.death_progress + self.death_rate * 10 * (self.size - self.stock:get('water'))
+    end
+    netr.log.f("water effects calculated", netr.log.band.consume)
+    local tmp = math.floor(self.growth_progress) - math.floor(self.death_progress)
+    self.growth_progress = self.growth_progress - math.floor(self.growth_progress)
+    self.death_progress = self.death_progress - math.floor(self.death_progress)
+    self.size = self.size + tmp
+    netr.log.f("size updated " .. self.size, netr.log.band.consume)
+    self.stock:add('plants', -tmp)
+    self.stock:add('water', -self.size)
+    if self.size <= 0 then
+        netr.log.f("sorry but someone has to die", netr.log.band.consume)
+        self.size = 0
+        self.growth_progress = 0
+        self.death_progress = 0
+        netr.bands_to_delete_counter = netr.bands_to_delete_counter + 1
+        netr.bands_to_delete[netr.bands_to_delete_counter] = self.id
+    end
+    netr.log.f("consume update finished", netr.log.band.consume)
+end
+
+function netr.Band:stock_update()
+    self.stock:set('plants', 0)
+    self.stock:set('water', 0)
 end
 
 function netr.Band:migration_update()
     netr.log.f("migration update", netr.log.band.migration)
-    if self.migration_target == nil then
+    if self.migration_target == nil and self.flags.not_enough_water then
         local target = nil
         netr.log.f("Band is looking for migration target", netr.log.band.migration)
         local possible_migration_targets = {}
@@ -265,14 +396,17 @@ function netr_main()
     
     netr.log.hsv_to_rgb = false
     netr.log.add_colour = false
-    netr.log.set_colour = true
+    netr.log.set_colour = false
     netr.log.update = true
-    netr.log.add_band = true
-    netr.log.draw = true
+    netr.log.add_band = false
+    netr.log.draw = false
     netr.log.band = {}
     netr.log.band.migration = false
     netr.log.band.init = false
-    netr.log.band.move = true
+    netr.log.band.move = false
+    netr.log.band.consume = false
+    netr.log.band.gather = false
+    netr.log.stock = false
     
     netr.flags = {}
     netr.flags.draw = true
@@ -282,16 +416,28 @@ function netr_main()
     
     netr.bands = {}
     netr.band_count = 0
+    netr.bands_to_delete = {}
+    netr.bands_to_delete_counter = 0
     
     if netr.flags.interface then
         log('netr is creating antiuser interface')
-        update_button = Buttons:LongText(ui)
+        netr.update_button = Buttons:LongText(ui)
             :reorient_top_left()
             :position(400, -400)
             :size(200, 200)
             :tooltip([[return "what"]])
             :text("what")
             :on_click([[netr.loop_step()]])
+        netr.population_label = Textbox:new(ui)
+            :reorient_top_left()
+            :position(100, -100)
+            :size(100, 100)
+            :text("total population")
+        netr.population_text = Textbox:new(ui)
+            :reorient_top_left()
+            :position(200, -100)
+            :size(100, 100)
+            :text("")
     end
     
     log('netr is loaded')
@@ -304,7 +450,7 @@ function on_tick()
         log("loop step")
         netr.loop_step()
     end
-    netr.tick = (netr.tick + 1) % 10
+    netr.tick = (netr.tick + 1) % 30
 end
 
 
